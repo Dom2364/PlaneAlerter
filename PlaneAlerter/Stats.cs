8c0901adbd44f3c42e16a11c3629857851a4413e
Dom2364 <djdom2364@gmail.com>
2017-01-06 08:32:48 +1000

    Added comments for Stats.cs

--- PlaneAlerter/Stats.cs
+++ PlaneAlerter/Stats.cs
@@ -1,23 +1,44 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace PlaneAlerter {
+	/// <summary>
+	/// Class for statistic operations
+	/// </summary>
 	static class Stats {
+		/// <summary>
+		/// Counter for total emails sent
+		/// </summary>
 		public static int totalEmailsSent = 0;
+
+		/// <summary>
+		/// Time planealerter started
+		/// </summary>
 		public static DateTime timeStarted = DateTime.Now;
+
+		/// <summary>
+		/// Number of conditions
+		/// </summary>
 		public static int numOfConditions {
 			get {
 				return Core.conditions.Count;
 			}
 		}
+
+		/// <summary>
+		/// Uptime of planealerter
+		/// </summary>
 		public static string uptime {
 			get {
 				TimeSpan difference = DateTime.Now.Subtract(timeStarted);
 				return difference.Hours + " Hrs " + difference.Minutes + " Mins " + difference.Seconds + " Secs";
 			}
 		}
+
+		/// <summary>
+		/// Method used in stats thread
+		/// </summary>
 		public static void updateStatsLoop() {
 			while (true) {
 				updateStats();
@@ -25,15 +46,19 @@ namespace PlaneAlerter {
 			}
 		}
 
+		/// <summary>
+		/// Update the UI with all the stats
+		/// </summary>
 		public static void updateStats() {
+			//Cancel if UI or other things are not in a state for displaying stuff
 			if (Core.UI.conditionTreeView.IsDisposed || Core.UI.conditionTreeView.Handle == null || ThreadManager.threadStatus == ThreadManager.CheckerStatus.Stopping)
 				return;
 			Core.UI.conditionTreeView.Invoke((MethodInvoker)delegate {
 				try {
-					Core.UI.conditionTreeView.Nodes[2].Nodes[0].Text = "Total Emails Sent: " + Stats.totalEmailsSent.ToString();
-					Core.UI.conditionTreeView.Nodes[2].Nodes[1].Text = "Total Conditions: " + Stats.numOfConditions.ToString();
-					Core.UI.conditionTreeView.Nodes[2].Nodes[2].Text = "Time Started: " + Stats.timeStarted.ToString();
-					Core.UI.conditionTreeView.Nodes[2].Nodes[3].Text = "Uptime: " + Stats.uptime.ToString();
+					Core.UI.conditionTreeView.Nodes[2].Nodes[0].Text = "Total Emails Sent: " + totalEmailsSent.ToString();
+					Core.UI.conditionTreeView.Nodes[2].Nodes[1].Text = "Total Conditions: " + numOfConditions.ToString();
+					Core.UI.conditionTreeView.Nodes[2].Nodes[2].Text = "Time Started: " + timeStarted.ToString();
+					Core.UI.conditionTreeView.Nodes[2].Nodes[3].Text = "Uptime: " + uptime.ToString();
 
 					foreach (TreeNode conditionNode in Core.UI.conditionTreeView.Nodes[0].Nodes) {
 						int conditionId = Convert.ToInt32(conditionNode.Nodes[0].Text.ToString().Substring(4));
