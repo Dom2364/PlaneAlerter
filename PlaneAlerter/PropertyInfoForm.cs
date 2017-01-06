21d6436930315634766acb2b1f7aca854de8dc8f
Dom2364 <djdom2364@gmail.com>
2017-01-06 15:25:56 +1000

    Removed unnessesary usings and added comments

--- PlaneAlerter/PropertyInfoForm.cs
+++ PlaneAlerter/PropertyInfoForm.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace PlaneAlerter {
 	public partial class PropertyInfoForm :Form {
 		public PropertyInfoForm() {
+			//Initialise form elements
 			InitializeComponent();
 
+			//Add vrs property info to form
 			foreach (Core.vrsProperty property in Core.vrsPropertyData.Keys) {
 				string[] propertyData = Core.vrsPropertyData[property];
                 propertyDataGridView.Rows.Add(property.ToString(), propertyData[0], propertyData[2], propertyData[3]);
