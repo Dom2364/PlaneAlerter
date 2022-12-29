a5686fc803659fa542199351febe424c678101ab
Dominic Hubert <djdom2364@gmail.com>
2022-12-30 00:15:26 +1000

    Twitter refactor

--- PlaneAlerter/Twitter.cs
+++ PlaneAlerter/Twitter.cs
@@ -11,44 +11,42 @@ using Tweetinvi.Exceptions;
 using Newtonsoft.Json;
 using PlaneAlerter.Forms;
 
-
 namespace PlaneAlerter {
 	/// <summary>
 	/// Class for twitter related stuff
 	/// </summary>
-	static class Twitter {
+	internal static class Twitter {
 		//Don't even think about it
 		private const string Key = "U1dYZUhZT0RqOG1hV25xRzczMXZ6Y3k3NA==";
 		private const string SecretKey = "UWlBcGtJaXJFdnpZaFhVb0FGVE93R003dnR0YnhJdWI1Y1BzVmxxSktuZGlmSkZvMzA=";
 
-		private static string ConsumerKey = Encoding.UTF8.GetString(Convert.FromBase64String(Key));
-		private static string ConsumerSecretKey = Encoding.UTF8.GetString(Convert.FromBase64String(SecretKey));
+		private static readonly string ConsumerKey = Encoding.UTF8.GetString(Convert.FromBase64String(Key));
+		private static readonly string ConsumerSecretKey = Encoding.UTF8.GetString(Convert.FromBase64String(SecretKey));
 
 		/// <summary>
 		/// Posts a tweet
 		/// </summary>
-		public static async Task<bool> Tweet(string token, string tokensecret, string content, string mediaURL) {
+		public static async Task<bool> Tweet(string token, string tokenSecret, string content, string mediaUrl) {
 			if (content.Contains("@")) {
 				Core.Ui.WriteToConsole("ERROR: Mentions are not allowed in tweets", System.Drawing.Color.Red);
 				return false;
 			}
 
 			//Set credentials
-			TwitterClient twitterClient = new TwitterClient(ConsumerKey, ConsumerSecretKey, token, tokensecret);
+			var twitterClient = new TwitterClient(ConsumerKey, ConsumerSecretKey, token, tokenSecret);
 
 			//Media ID of attached image
-			long? mediaid = null;
+			long? mediaId = null;
 
 			//Upload map image if required
-			if (mediaURL != "") {
-				MemoryStream ms = new MemoryStream();
+			if (mediaUrl != "") {
+				var ms = new MemoryStream();
 
 				try {
 					//Download the map image from google
-					HttpWebRequest req = (HttpWebRequest)WebRequest.Create(mediaURL);
-					using (WebResponse res = req.GetResponse()) {
-						res.GetResponseStream().CopyTo(ms);
-					}	
+					var req = (HttpWebRequest)WebRequest.Create(mediaUrl);
+					using var res = req.GetResponse();
+					await res.GetResponseStream().CopyToAsync(ms);
 				}
 				catch (Exception e) {
 					Core.Ui.WriteToConsole($"ERROR: {e.GetType()} error downloading map image from Google: {e.Message}", System.Drawing.Color.Red);
@@ -57,16 +55,16 @@ namespace PlaneAlerter {
 
 				try {
 					//Upload it to twitter
-					byte[] imageBytes = ms.ToArray();
-					IMedia media = await twitterClient.Upload.UploadTweetImageAsync(imageBytes);
-					ms.Dispose();
+					var imageBytes = ms.ToArray();
+					var media = await twitterClient.Upload.UploadTweetImageAsync(imageBytes);
+					await ms.DisposeAsync();
 
 					//Check if it was uploaded properly
-					if (media.HasBeenUploaded) mediaid = media.Id;
+					if (media.HasBeenUploaded) mediaId = media.Id;
 					else Core.Ui.WriteToConsole("ERROR: Error uploading map", System.Drawing.Color.Red);
 				}
 				catch (TwitterException e) {
-					Core.Ui.WriteToConsole($"ERROR: Error uploading map image to Twitter: {e.ToString()}", System.Drawing.Color.Red);
+					Core.Ui.WriteToConsole($"ERROR: Error uploading map image to Twitter: {e}", System.Drawing.Color.Red);
 				}
 				catch (Exception e) { 
 					Core.Ui.WriteToConsole($"ERROR: {e.GetType()} error uploading map image to Twitter: {e.Message}", System.Drawing.Color.Red);
@@ -75,28 +73,28 @@ namespace PlaneAlerter {
 
 			//Publish tweet
 			try {
-				string requestbody = null;
-				if (mediaid != null) {
-					requestbody = $@"{{""text"": ""{content}"", ""media"": {{""media_ids"": [""{mediaid}""]}}}}";
+				string requestBody;
+				if (mediaId != null) {
+					requestBody = $@"{{""text"": ""{content}"", ""media"": {{""media_ids"": [""{mediaId}""]}}}}";
 				}
 				else {
-					requestbody = $@"{{""text"": ""{content}""}}";
+					requestBody = $@"{{""text"": ""{content}""}}";
 				}
-				var tweet = await twitterClient.Execute.RequestAsync(request =>
+				await twitterClient.Execute.RequestAsync(request =>
 				{
 					request.Url = "https://api.twitter.com/2/tweets";
 					request.HttpMethod = HttpMethod.POST;
-					request.HttpContent = new System.Net.Http.StringContent(requestbody, Encoding.ASCII, "application/json");
+					request.HttpContent = new System.Net.Http.StringContent(requestBody, Encoding.ASCII, "application/json");
 				});
 			}
 			catch (TwitterException e) {
-				string detailedcontent = null;
+				string? detailedContent = null;
 				if (e.Content != null) {
-					Newtonsoft.Json.Linq.JObject contentjson = JsonSerializer.Create().Deserialize<Newtonsoft.Json.Linq.JObject>(new JsonTextReader(new StringReader(e.Content)));
-					detailedcontent = contentjson["detail"]!=null? contentjson["detail"].ToString() : null;
+					var contentJson = JsonSerializer.Create().Deserialize<Newtonsoft.Json.Linq.JObject>(new JsonTextReader(new StringReader(e.Content)));
+					detailedContent = contentJson["detail"]?.ToString();
 				}
 
-				Core.Ui.WriteToConsole($"ERROR: Error publishing tweet: {(detailedcontent!=null?detailedcontent + Environment.NewLine:"")}{e.ToString()}", System.Drawing.Color.Red);
+				Core.Ui.WriteToConsole($"ERROR: Error publishing tweet: {(detailedContent != null ? detailedContent + Environment.NewLine : "")}{e.ToString()}", System.Drawing.Color.Red);
 				return false;
 			}
 			catch (Exception e) {
@@ -111,7 +109,7 @@ namespace PlaneAlerter {
 		/// Authenticate account and add to accounts list
 		/// </summary>
 		public static async void AddAccount() {
-			TwitterClient appClient = new TwitterClient(ConsumerKey, ConsumerSecretKey);
+			var appClient = new TwitterClient(ConsumerKey, ConsumerSecretKey);
 
 			//Authenticate PlaneAlerter
 			IAuthenticationRequest authenticationRequest;
@@ -135,58 +133,59 @@ namespace PlaneAlerter {
 			Process.Start(new ProcessStartInfo(authenticationRequest.AuthorizationURL) { UseShellExecute = true });
 
 			//Show dialog for user to enter pin
-			PinPromptDialog dialog = new PinPromptDialog();
-			if (dialog.ShowDialog() == DialogResult.OK) {
-				//Get credentials from Twitter
-				ITwitterCredentials userCredentials;
-				try {
-					userCredentials = await appClient.Auth.RequestCredentialsFromVerifierCodeAsync(dialog.PIN, authenticationRequest);
-				}
-				catch (TwitterAuthException e) {
-					MessageBox.Show("Auth error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
-					return;
-				}
-				catch (TwitterException e) {
-					MessageBox.Show("Error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
-					return;
-				}
-				catch (Exception e) {
-					MessageBox.Show($"{e.GetType()} error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
-					return;
-				}
-
-				//Set active credentials
-				TwitterClient userClient = new TwitterClient(userCredentials);
-				IAuthenticatedUser user = await userClient.Users.GetAuthenticatedUserAsync();
-				//Get screen name
-				string screenname = user.ScreenName;
-
-				//Check if users list already contains user that was just authenticated
-				if (Settings.TwitterUsers.ContainsKey(screenname)) {
-					MessageBox.Show("User '" + screenname + "' already added", "User already added");
-					return;
-				}
-
-				//Add user
-				Settings.TwitterUsers.Add(screenname, new string[] { userCredentials.AccessToken, userCredentials.AccessTokenSecret });
-				MessageBox.Show("Twitter user '" + screenname + "' authorized!", "User Authorized");
-				Core.Ui.UpdateTwitterAccounts();
+			var dialog = new PinPromptDialog();
+			if (dialog.ShowDialog() != DialogResult.OK)
+				return;
+			
+			//Get credentials from Twitter
+			ITwitterCredentials userCredentials;
+			try {
+				userCredentials = await appClient.Auth.RequestCredentialsFromVerifierCodeAsync(dialog.PIN, authenticationRequest);
 			}
+			catch (TwitterAuthException e) {
+				MessageBox.Show("Auth error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
+				return;
+			}
+			catch (TwitterException e) {
+				MessageBox.Show("Error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
+				return;
+			}
+			catch (Exception e) {
+				MessageBox.Show($"{e.GetType()} error while retrieving user credentials: " + Environment.NewLine + Environment.NewLine + e.ToString(), "Authentication Error");
+				return;
+			}
+
+			//Set active credentials
+			var userClient = new TwitterClient(userCredentials);
+			var user = await userClient.Users.GetAuthenticatedUserAsync();
+			//Get screen name
+			var screenName = user.ScreenName;
+
+			//Check if users list already contains user that was just authenticated
+			if (Settings.TwitterUsers.ContainsKey(screenName)) {
+				MessageBox.Show("User '" + screenName + "' already added", "User already added");
+				return;
+			}
+
+			//Add user
+			Settings.TwitterUsers.Add(screenName, new[] { userCredentials.AccessToken, userCredentials.AccessTokenSecret });
+			MessageBox.Show("Twitter user '" + screenName + "' authorized!", "User Authorized");
+			Core.Ui.UpdateTwitterAccounts();
 		}
 
 		/// <summary>
 		/// Remove a specific account
 		/// </summary>
-		public static void RemoveAccount(string screenname) {
+		public static void RemoveAccount(string screenName) {
 			//Check if users list contains user
-			if (!Settings.TwitterUsers.ContainsKey(screenname)) {
-				MessageBox.Show("User '" + screenname + "' does not exist", "User doesn't exist");
+			if (!Settings.TwitterUsers.ContainsKey(screenName)) {
+				MessageBox.Show("User '" + screenName + "' does not exist", "User doesn't exist");
 				return;
 			}
 
 			//Remove user
-			Settings.TwitterUsers.Remove(screenname);
-			MessageBox.Show("Twitter user '" + screenname + "' removed!", "User Removed");
+			Settings.TwitterUsers.Remove(screenName);
+			MessageBox.Show("Twitter user '" + screenName + "' removed!", "User Removed");
 			Core.Ui.UpdateTwitterAccounts();
 		}
 	}
